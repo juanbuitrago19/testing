594222c3e96432088abecf2ec2c4c1f6572c1991
juanbuitrago19 <juanbuitrago.fkj@gmail.com>
2023-04-26 02:29:42 -0700

    update-19

--- js/JS-Principal.js
+++ js/JS-Principal.js
@@ -5,48 +5,414 @@ let products = {
         brand: "Beauty Glazed",
         category: "Sombras",
         price: "60.000",
+        stock: true,
         discount: true,
         newProduct: true,
         image: "product-0000001.jpg",
-        URL: ""
+        url: "title-product-1.html"
       },
       {
         productName: "Paleta color studio ",
         brand: "Beauty Glazed",
         category: "Sombras",
         price: "50.000",
+        stock: true,
         discount: true,
         newProduct: true,
         image: "product-0000002.jpg",
+        url: "title-product-2.html"
       },
       {
-        productName: "Paleta hawaii",
+        productName: "Paleta sombras hawaii",
         brand: "Atenea",
         category: "Sombras",
         price: "78.000",
+        stock: true,
         discount: true,
         newProduct: true,
         image: "product-0000003.jpg",
+        url: "title-product-1.html"
       },
       {
         productName: "Paleta majestic",
         brand: "Atenea",
         category: "Sombras",
         price: "70.000",
+        stock: true,
         discount: true,
         newProduct: true,
         image: "product-0000004.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Paleta sombras birds",
+        brand: "Atenea",
+        category: "Sombras",
+        price: "80.000",
+        stock: true,
+        discount: true,
+        newProduct: true,
+        image: "product-0000143.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Paleta sombras marvelous",
+        brand: "Atenea",
+        category: "Sombras",
+        price: "70.000",
+        stock: true,
+        discount: false,
+        newProduct: false,
+        image: "product-0000144.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Paleta art deco",
+        brand: "Atenea",
+        category: "Sombras",
+        price: "37.000",
+        stock: true,
+        discount: true,
+        newProduct: true,
+        image: "product-0000005.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Paleta perfect mix",
+        brand: "Beauty Glazed",
+        category: "Sombras",
+        price: "35.000",
+        stock: true,
+        discount: true,
+        newProduct: true,
+        image: "product-0000006.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "paleta hono lulu",
+        brand: "Atenea",
+        category: "Sombras",
+        price: "55.000",
+        stock: true,
+        discount: true,
+        newProduct: true,
+        image: "product-0000007.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "paleta Perfect Neutral",
+        brand: "Beauty Glazed",
+        category: "Sombras",
+        price: "29.000",
+        stock: true,
+        discount: true,
+        newProduct: true,
+        image: "product-0000008.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Paleta Mystic Glow",
+        brand: "Ruby Rose",
+        category: "Sombras",
+        price: "48.000",
+        stock: true,
+        discount: true,
+        newProduct: true,
+        image: "product-0000009.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Paleta Sublime",
+        brand: "Atenea",
+        category: "Sombras",
+        price: "42.000",
+        stock: true,
+        discount: true,
+        newProduct: true,
+        image: "product-0000010.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Paleta Atemporal",
+        brand: "Atenea",
+        category: "Sombras",
+        price: "70.000",
+        stock: true,
+        discount: true,
+        newProduct: true,
+        image: "product-0000011.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Paleta thunder (Tono 6)",
+        brand: "Ushas",
+        category: "Sombras",
+        price: "15.000",
+        stock: true,
+        discount: true,
+        newProduct: true,
+        image: "product-0000012.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Paleta frutas (Fresa)",
+        brand: "Trendy",
+        category: "Sombras",
+        price: "15.000",
+        stock: true,
+        discount: true,
+        newProduct: true,
+        image: "product-0000013.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Paleta frutas (uva)",
+        brand: "Trendy",
+        category: "Sombras",
+        price: "15.000",
+        stock: true,
+        discount: true,
+        newProduct: true,
+        image: "product-0000014.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Paleta frutas (Coco)",
+        brand: "Trendy",
+        category: "Sombras",
+        price: "15.000",
+        stock: true,
+        discount: true,
+        newProduct: true,
+        image: "product-0000015.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Paleta frutas (Durazno)",
+        brand: "Trendy",
+        category: "Sombras",
+        price: "15.000",
+        stock: true,
+        discount: true,
+        newProduct: true,
+        image: "product-0000016.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Sombra liquida Sublime (Tono 2)",
+        brand: "Atenea",
+        category: "Sombras",
+        price: "25.000",
+        stock: true,
+        discount: true,
+        newProduct: false,
+        image: "product-0000017VAR1.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Sombra liquida Sublime (Tono 3)",
+        brand: "Atenea",
+        category: "Sombras",
+        price: "25.000",
+        stock: true,
+        discount: true,
+        newProduct: false,
+        image: "product-0000017VAR2.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Paleta Blusher",
+        brand: "Atenea",
+        category: "Rubores",
+        price: "30.000",
+        stock: true,
+        discount: true,
+        newProduct: true,
+        image: "product-0000018.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Paleta Divine Baked",
+        brand: "Atenea",
+        category: "Iluminadores",
+        price: "38.000",
+        stock: true,
+        discount: true,
+        newProduct: true,
+        image: "product-0000019.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Iluminador Baked powder (Tono rosa)",
+        brand: "Atenea",
+        category: "Iluminadores",
+        price: "20.000",
+        stock: true,
+        discount: true,
+        newProduct: true,
+        image: "product-0000020VAR1.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Iluminador Baked powder (Tono bage)",
+        brand: "Atenea",
+        category: "Iluminadores",
+        price: "20.000",
+        stock: true,
+        discount: true,
+        newProduct: true,
+        image: "product-0000020VAR2.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Iluminador corporal (Tono bronce)",
+        brand: "Elaya",
+        category: "Iluminadores",
+        price: "27.000",
+        stock: true,
+        discount: true,
+        newProduct: true,
+        image: "product-0000021VAR1.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Iluminador corporal (Tono plata)",
+        brand: "Elaya",
+        category: "Iluminadores",
+        price: "27.000",
+        stock: true,
+        discount: true,
+        newProduct: true,
+        image: "product-0000021VAR2.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Iluminador corporal (Tono oro rosa)",
+        brand: "Elaya",
+        category: "Iluminadores",
+        price: "27.000",
+        stock: true,
+        discount: true,
+        newProduct: true,
+        image: "product-0000021VAR3.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Iluminador liquido (Tono champaña)",
+        brand: "Elaya",
+        category: "Iluminadores",
+        price: "15.000",
+        stock: true,
+        discount: true,
+        newProduct: false,
+        image: "product-0000022VAR1.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Iluminador liquido (Tono dorado)",
+        brand: "Elaya",
+        category: "Iluminadores",
+        price: "15.000",
+        stock: true,
+        discount: true,
+        newProduct: false,
+        image: "product-0000022VAR2.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Iluminador liquido (Tono rosa)",
+        brand: "Elaya",
+        category: "Iluminadores",
+        price: "15.000",
+        stock: true,
+        discount: true,
+        newProduct: false,
+        image: "product-0000022VAR3.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Paleta iluminadores y contornos girl",
+        brand: "Ushas",
+        category: "Iluminadores",
+        price: "19.000",
+        stock: true,
+        discount: true,
+        newProduct: false,
+        image: "product-0000023.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Paleta cloud",
+        brand: "Trendy",
+        category: "Iluminadores",
+        price: "15.000",
+        stock: true,
+        discount: true,
+        newProduct: false,
+        image: "product-0000024.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Iluminador eclipse lunar",
+        brand: "Trendy",
+        category: "Iluminadores",
+        price: "12.000",
+        stock: true,
+        discount: true,
+        newProduct: false,
+        image: "product-0000025.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Polvo de hadas (Tono rosa)",
+        brand: "Dolce Bella",
+        category: "Iluminadores",
+        price: "15.000",
+        stock: true,
+        discount: true,
+        newProduct: false,
+        image: "product-0000026VAR1.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Polvo de hadas (Tono cobre)",
+        brand: "Dolce Bella",
+        category: "Iluminadores",
+        price: "15.000",
+        stock: true,
+        discount: true,
+        newProduct: false,
+        image: "product-0000026VAR2.jpg",
+        url: "title-product-2.html"
+      },
+      {
+        productName: "Polvo de hadas (Tono oro)",
+        brand: "Dolce Bella",
+        category: "Iluminadores",
+        price: "15.000",
+        stock: true,
+        discount: true,
+        newProduct: false,
+        image: "product-0000027VAR3.jpg",
+        url: "title-product-2.html"
       },
     ],
   };
 
 
   let productsContainer = document.querySelector("#products");
+  let newProductsCount = 0;
+
+
     products.data.forEach(product => {
+
+        if (product.newProduct) {
+
+        newProductsCount++;
 
         let cardProduct = document.createElement("div");
         cardProduct.classList.add("slide", "card-product");
-        cardProduct.setAttribute("onclick", "window.location.href='bases.html';");
+        cardProduct.setAttribute("onclick", `window.location.href='products/${product.url}';`);
 
         let imageSection = document.createElement("div");
         imageSection.classList.add("image-section");
@@ -63,8 +429,9 @@ let products = {
         let title = document.createElement("span");
         title.textContent = product.productName;
         titlePrice.appendChild(title);
+
         let priceSpan = document.createElement("span");
-        priceSpan.textContent = product.price;
+        priceSpan.textContent = "$" + product.price;
         titlePrice.appendChild(priceSpan);
         infoSection.appendChild(titlePrice);
 
@@ -83,6 +450,11 @@ let products = {
         cardProduct.appendChild(imageSection);
         cardProduct.appendChild(infoSection);
         productsContainer.appendChild(cardProduct);
+
+        }
 });
+
+let numNewProductsContainer = document.querySelector("#numNewProducts");
+numNewProductsContainer.innerHTML = newProductsCount + " nuevos productos";
   
 
