9f4d0ec78e88baafe2d7bdefe109fdb4c62d8212
juanbuitrago19 <juanbuitrago.fkj@gmail.com>
2023-05-08 06:27:04 -0700

    huugh777

--- js/app7.js
+++ js/app7.js
@@ -1,17 +1,17 @@
-const cards = document.querySelectorAll(".slide");
+const cards = document.querySelectorAll(".product--card");
 
 cards.forEach((slide) => {
     const buttonSection = slide.querySelector(".product-detail-btn");
 
     slide.addEventListener("mouseover", () => {
         buttonSection.style.transition = "0.5s ease";
-        buttonSection.style.transform = "translate( 5px, 0px)";
+        buttonSection.style.transform = "translate( 0px, 0px)";
         buttonSection.style.visibility = "visible";
     });
 
     slide.addEventListener("mouseout", () => {
         buttonSection.style.transition = "0s ease";
-        buttonSection.style.transform = "translate( -5px, 0px)";
+        buttonSection.style.transform = "translate( -10px, 0px)";
         buttonSection.style.visibility = "hidden";
     });
 });
