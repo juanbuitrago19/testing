bfc16100eea48f8f2b997727246f952b749b39fb
juanbuitrago19 <juanbuitrago.fkj@gmail.com>
2023-05-11 01:27:42 -0700

    d3d3

--- js/app12.js
+++ js/app12.js
@@ -18,3 +18,25 @@ brandElements.forEach(function(element) {
     link.classList.remove('hover-line');
   });
 });
+
+
+// Obtener todos los elementos li con la clase "brand__element"
+var brandElements2 = document.querySelectorAll('.brand--card--grid');
+
+// Iterar sobre cada elemento li
+brandElements2.forEach(function(element) {
+  // Obtener el enlace "Ver todo" dentro del elemento li actual
+  var link = element.querySelector('.cta__view__page_2');
+
+  // Agregar un evento de escucha para el evento de hover
+  element.addEventListener('mouseover', function() {
+    // Agregar la clase "hover-line" al enlace "Ver todo"
+    link.classList.add('hover-line');
+  });
+
+  // Agregar un evento de escucha para el evento de dejar de hacer hover
+  element.addEventListener('mouseout', function() {
+    // Eliminar la clase "hover-line" del enlace "Ver todo"
+    link.classList.remove('hover-line');
+  });
+});
---
+++ js/marcas-grid-page.js
@@ -0,0 +1,51 @@
+import { products } from "./Array-Objects.js";
+
+let var1 = document.querySelector("#show--brands");
+let newProductsCount = 0;
+
+let marcas = [...new Set(products.data.map(product => product.brand))];
+
+marcas.forEach(marca => {
+
+    newProductsCount++;
+
+    let count = products.data.filter(product => product.brand === marca).length;
+    let brandUrl = products.data.filter(product => product.brand === marca)[0].brandUrl;
+
+
+    let liBrandElement = document.createElement("div");
+    liBrandElement.classList.add("brand--card--grid");
+    liBrandElement.setAttribute("onclick", `window.location.href='${brandUrl}';`);
+
+
+    let h2CellTitle = document.createElement("h2");
+    h2CellTitle.classList.add("cell__title_2");
+    h2CellTitle.textContent = marca;
+    liBrandElement.appendChild(h2CellTitle);
+
+    let divAlignItemsRight = document.createElement("div");
+    divAlignItemsRight.classList.add("align_items_right_2");
+
+    let aNumProducts = document.createElement('a');
+    aNumProducts.classList.add("num__products_2");
+    aNumProducts.textContent = "("+ count + ")" + "Productos";
+
+    let aCtaViewPage = document.createElement("a");
+    aCtaViewPage.classList.add("cta__view__page_2");
+    aCtaViewPage.href = brandUrl;
+    aCtaViewPage.innerText = 'Ver todo';
+
+    divAlignItemsRight.appendChild(aNumProducts);
+    divAlignItemsRight.appendChild(aCtaViewPage);
+
+    liBrandElement.appendChild(divAlignItemsRight);
+
+    var1.appendChild(liBrandElement);
+
+});
+
+let contador = document.querySelector("#numNewProducts");
+contador.innerHTML = newProductsCount + " Productos";
+
+
+
